e61d8dbc60f7aafe39d729562c2780652438e858
simon <simonhanwilson@icloud.com>
2019-09-23 18:46:16 +0100

    added intent to transition from first to second activity

--- app/src/main/java/com/example/androidlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/androidlifecycle/MainActivity.kt
@@ -1,13 +1,23 @@
 
 package com.example.androidlifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedButton.setOnClickListener {
+
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+
+            startActivity(leagueIntent)
+
+        }
     }
 }
